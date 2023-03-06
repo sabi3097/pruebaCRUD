b33fcdb1495356a02e2ea899b4b92e24c05488cc
sabi3097 <sabi30970@gmail.com>
2023-03-06 16:59:29 +0000

    se añaden estructuras de modelos y contraladores para las tablas de la base de datos

---
+++ app/Http/Controllers/ProProcesoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\pro_proceso;
+use Illuminate\Http\Request;
+
+class ProProcesoController extends Controller
+{
+    /**
+     * pagina de inicio
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * formulario donde se agregan datos
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * guarda los datos
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * obtener un registro de la tabla
+     */
+    public function show(pro_proceso $pro_proceso)
+    {
+        //
+    }
+
+    /**
+     * Lista los registros a editar
+     */
+    public function edit(pro_proceso $pro_proceso)
+    {
+        //
+    }
+
+    /**
+     * editar los datos
+     */
+    public function update(Request $request, pro_proceso $pro_proceso)
+    {
+        //
+    }
+
+    /**
+     * elimina datos
+     */
+    public function destroy(pro_proceso $pro_proceso)
+    {
+        //
+    }
+}
