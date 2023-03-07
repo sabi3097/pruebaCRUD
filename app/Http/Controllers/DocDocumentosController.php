bdd0a66e79fc87c62a6f5414a863bce4c8ed4faa
sabi3097 <sabi30970@gmail.com>
2023-03-07 00:26:12 +0000

    se agregan campos a las migraciones y se realiza una migracion a la base de datos

---
+++ app/Http/Controllers/DocDocumentosController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\doc_documentos;
+use Illuminate\Http\Request;
+
+class DocDocumentosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(doc_documentos $doc_documentos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(doc_documentos $doc_documentos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, doc_documentos $doc_documentos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(doc_documentos $doc_documentos)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ProProcesosController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\pro_procesos;
+use Illuminate\Http\Request;
+
+class ProProcesosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(pro_procesos $pro_procesos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(pro_procesos $pro_procesos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, pro_procesos $pro_procesos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(pro_procesos $pro_procesos)
+    {
+        //
+    }
+}
